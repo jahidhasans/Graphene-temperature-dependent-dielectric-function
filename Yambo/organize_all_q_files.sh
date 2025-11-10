f39d3d981369407eb6f0e8c54669dbb42a8e9343
jahidhasans <119380627+jahidhasans@users.noreply.github.com>
2025-11-10 13:54:00 -0500

    Update organize_all_q_files.sh

--- Yambo/organize_all_q_files.sh
+++ Yambo/organize_all_q_files.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 folder="eps"  #change the folder name according to yours
 #folder="./"
 expected_total=729
-
 missing_count=0
 empty_count=0
 found_count=0
@@ -28,10 +26,7 @@ for i in $(seq 1 $expected_total); do
     fi
 done
 
-# Print summary
-echo "------------------------------------"
 echo "Total expected files     : $expected_total"
 echo "Total files found        : $found_count"
 echo "Total missing files      : $missing_count"
 echo "Total empty files        : $empty_count"
-echo "------------------------------------"
